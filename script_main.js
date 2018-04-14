47418e567450f48d60f15ac2962c56f25054d12e
krustamov <32468709+krustamov@users.noreply.github.com>
2018-04-14 02:11:15 -0500

    second version

--- script_main.js
+++ script_main.js
@@ -35,8 +35,8 @@ let parameters = {
     filtered_data: null
 };
 
-let tableTest = d3.select('#ufo-list');
-let louisTest = d3.select('#dateInput');
+let tableUFO = d3.select('#ufo-list');
+let singleDate = d3.select('#dateInput');
 
 function handleSubmitClick() {
     let selectValue = d3.select('#dateInput')
@@ -57,60 +57,10 @@ let submitBtn = document.querySelector('#search-ufo');
 
 submitBtn.addEventListener('click', handleSubmitClick);
 
-// function handleSubmitClick() {
-//     // The default behavior of a button clicked inside of a form is to try to submit the form somewhere (which we don't want)
-//     // event.preventDefault();
-//     console.log(document.getElementById('dateInput').value)
-// }
-
-
-
-// function loadDropDown() {
-//     let dropDownOptions = new Set();
-//     dropDownOptions.add('All');
-//     dropDownOptions.add('35000');
-//     dropDownOptions.add('60000');
-//     dropDownOptions.add('80000');
-//     dropDownOptions.add('100000');
-
-//     incomeDropDown.on('change', onchange);
-
-//     let options = incomeDropDown
-//         .selectAll('option')
-//         .data([...dropDownOptions])
-//         .enter()
-//         .append('option')
-//         .attr('value', function (d) { return d; })
-//         .text(function (d) { return d; });
-
-// }
-
 function onchange() {
     // clean filtered data
     parameters.filtered_data = [];
 
-    // run through my data
-    // if my data.shape == whatever shape is in input
-    // add my data to filtered_data
-
-
-    // grab whatever stuff they enter..
-    // append to parameters.data 
-
-
-    // let selectValue = d3.select('#income')
-    //     .property('value');
-    // // console.log(selectValue);
-    // 
-    // for (let row of parameters.data) {
-    //     if (selectValue === 'All') {
-    //         parameters.filtered_data.push(row);
-    //     }
-    //     else if (parseFloat(row['Household Income']) >= parseFloat(selectValue)) {
-    //         parameters.filtered_data.push(row);
-    //     }
-    // }
-    //console.log(parameters.filtered_data);
     createTables();
 }
 
@@ -119,7 +69,6 @@ function init(ufoData) {
     parameters.data = ufoData;
     parameters.filtered_data = ufoData;
     createTables();
-    // loadDropDown();
 }
 
 let ufoList = d3.select('#ufo-list');
@@ -159,224 +108,39 @@ function createTables() {
 
 }
 
+//from here
 
+// let tableUFOMulti = d3.select('#ufo-list');
+// let multipleDate = d3.select("#dateInput1");
+// // // // let UFOInput2 = d3.select("#ufo-city");
+// // // // let UFOInput3 = d3.select("#ufo-state");
+// // // // let UFOInput4 = d3.select("#ufo-country");
+// // // // let UFOInput5 = d3.select("#ufo-shape");
 
-
-
-// 
-
-//     table.append('thead').append('tr')
-//         .selectAll('th')
-//         .data(parameters['columns'])
-//         .enter()
-//         .append('th')
-//         .text(function (data) { return data.title; });
-
-//     table.append('tbody')
-//         .selectAll('tr') // create row for each row of data
-//         .data(ufoData)
-//         .enter()
-//         .append('tr')
-//         .selectAll('td')
-//         .data(function (row, i) {
-//             // evaluate column objects against the current row
-//             return parameters.columns.map(function (column) {
-//                 var cell = {};
-//                 d3.keys(column).forEach(function (k) {
-//                     if (typeof (column[k]) === 'function') {
-//                         cell[k] = column[k](row, i)
-//                     }
-//                 });
-//                 return cell;
-//             });
-//         }).enter()
-//         .append('td')
-//         .text(function (data) { return data.html; });
-
-// });
-
-
-//borrowed codes
-
-var spanPrev = document.getElementById("#prev");
-var spanNext = document.getElementById("#next");
-var spanFirst = document.getElementById("#first");
-var spanLast = document.getElementById("#last");
-
-// Pagination vars
-var curr_row = 0; //page number
-var rowsperpage = 50;
-var pan_pos = 1;
-var len = 0;
-
-// Single
-var addUFOBtn = document.getElementById("#add-ufo");
-var UFOInput = document.getElementById("#ufo-input");
-
-// Multi vars
-var addUFOMultiBtn = document.getElementById("#add-ufo2");
-var UFOInput1 = document.getElementById("#ufo-input1");
-var UFOInput2 = document.getElementById("#ufo-input2");
-var UFOInput3 = document.getElementById("#ufo-input3");
-var UFOInput4 = document.getElementById("#ufo-input4");
-var UFOInput5 = document.getElementById("#ufo-input5");
-
-var ddtime = "";
-var city = "";
-var state = "";
-var country = "";
-var shape = "";
-
-// Table tag
-var UFOList = document.querySelector("#ufo-list");
-
-var SearchData = []
-
-function showTable() {
-
-    // Extract value for Html Header
-    if ((SearchData.length / rowsperpage) > parseInt(SearchData.length / rowsperpage)) {
-        len = parseInt(SearchData.length / rowsperpage) + 1;
-    }
-    else {
-        len = parseInt(SearchData.length / rowsperpage);
-    }
-
-    UFOList.innerHTML = "";
-    var col = [];
-    for (var i = 0; i < SearchData.length; i++) {
-        for (var key in SearchData[i]) {
-            if (col.indexOf(key) === -1) {
-                col.push(key);
-            }
-        }
-    }
-
-    // CREATE DYNAMIC TABLE.
-    var table = document.createElement("table");
-
-    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED
-
-    var tr = table.insertRow(-1);
-
-    for (var i = 0; i < col.length; i++) {
-        var th = document.createElement("th");
-        th.innerHTML = col[i];
-        UFOList.appendChild(th);
-    }
-
-    var inpage_row = 0;
-
-    // ADD JSON DATA TO THE TABLE AS ROWS.
-    for (var i = curr_row; i < SearchData.length; i++)
-
-        tr = table.insertRow(-1);
-    UFOList.appendChild(tr);
-
-    if (inpage_row < rowsperpage) {
-        for (var j = 0; j < col.length; j++) {
-            var tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = SearchData[i][col[j]];
-            UFOList.appendChild(tabCell);
-        }
-        inpage_row = inpage_row + 1;
-    }
-    else {
-        curr_row = i;
-        break
-    }
-};
-
-// function createUFOtb1() {
-
-//     curr_row = 0;
-
-//     pan_pos = 1;
-//     SearchData = [];
-//     for (var i = 0; i < UFOdata.length; i++) {
-//         SearchData.push(UFOdata[i]);
-//     }
-
-//     showTable();
-// }
-
-// addUFOBtn.addEventListener("click", function (event) {
-
-//     curr_row = 0;
-//     pan_pos = 1;
-//     dttime = UFOInput.value;
-//     SearchData = [];
-//     for (var i = 0; i < UFOdata.length; i++) {
-//         if (UFOdata[i]["datetime"] == dttime || dttime == "") {
-//             SearchData.push(UFOdata[i]);
+// function handleSubmitClick() {
+//     let selectValue = d3.select('#dateInput1')
+//         .property('value');
+//     // console.log(selectValue);
+//     parameters.filtered_data = [];
+//     for (let row of ufoData) {
+//         if (selectValue === row.datetime) {
+//             parameters.filtered_data.push(row);
 //         }
-
 //     }
-
-//     UFOInput.value = "";
-//     showTable();
-
-// });
-
-// addUFOMultiBtn.addEventListener("click", function (event) {
-
-//     curr_row = 0;
-//     pan_pos = 1;
-//     dttime = UFOInput1.value;
-//     city = UFOInput2.value;
-//     state = UFOInput3.value;
-//     country = UFOInput4.value;
-//     shape = UFOInput5.value;
-
-//     SearchData = [];
-//     for (var i = 0; i < UFOdata.length; i++) {
-//         if ((UFOdata[i]["datetime"] == dttime || dttime) &&
-//             (UFOdata[i]["city"].toUpperCase() == city.toUpperCase() || city == "") &&
-//             (UFOdata[i]["state"].toUpperCase() == state.toUpperCase() || state == "") &&
-//             (UFOdata[i]["country"].toUpperCase() == country.toUpperCase() || country== "") &&
-//     (UFOdata[i]["shape"].toUpperCase() == shape.toUpperCase() || shape == "")
-//             )
-// {
-//     SearchData.push(UFOdata[i]);
+//     // console.log(parameters.filtered_data);
+//     createTables();
 // }
-//   }
-// UFOInput1.value = "";
-// UFOInput2.value = "";
-// UFOInput3.value = "";
-// UFOInput4.value = "";
-// UFOInput5.value = "";
-// showtable();
-// });
 
-// spanPrev.addEventListener("click", function (event) {
-//     if (pan_pos > 1) {
-//         pan_pos -= 1;
-//     }
-//     curr_row = (pan_pos * rowsperpage) - rowsperpage;
-//     showtable()
-// });
 
-// spanNext.addEventListener("click", function (event) {
-//     if (pan_pos >= len) {
-//         pan_pos = len;
-//     }
-//     else {
-//         pan_pos += 1;
-//     }
-//     curr_row = (pan_pos * rowsperpage) - rowsperpage;
-//     showtable()
-// });
+// let submitBtn2 = document.querySelector('#search-ufo2');
 
-// spanFirst.addEventListener("click", function (event) {
-//     pan_pos = 1;
-//     curr_row = (pan_pos * rowsperpage) - rowsperpage;
-//     showtable()
-// });
+// submitBtn2.addEventListener('click', handleSubmitClick);
 
-// spanLast.addEventListener("click", function (event) {
-//     pan_pos = len;
-//     curr_row = (pan_pos * rowsperpage) - rowsperpage;
-//     showtable()
-// });
+// function onchange() {
+//     // clean filtered data
+//     parameters.filtered_data = [];
 
-// createUFOtb1();
+//     createTables();
+// }
+
+//to here
